a45634a48221a54cf5e34a562cf66e82f8de412d
uiux@okdollar <uiux@okdollar.com>
2025-08-01 14:34:25 +0530

    get all items swagger

--- src/routes/itemRoute.js
+++ src/routes/itemRoute.js
@@ -5,6 +5,78 @@ import Items from '../models/itemSchema.js';
 
 
 const router = express.Router()
+
+/**
+ * @swagger
+ * tags:
+ *  name : Items
+ *  description : Items Management APIs 
+ */
+
+/**
+ * @swagger
+ * /items:
+ *   post:
+ *     summary: Create a new Item
+ *     tags: [Items]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               itemName:
+ *                 type: string
+ *                 example: Remi Note 14
+ *               category:
+ *                  type: string
+ *                  example: 129392939939
+ *               company:
+ *                  type: string 
+ *                  example: 129393
+ *               price:
+ *                  type: number
+ *                  example: 1500000
+ *               color:
+ *                  type: string 
+ *                  example: blue
+ *               option:
+ *                  type: string
+ *                  example: Ram 4 , 16 Gb
+ *              
+ *     responses:
+ *       201:
+ *         description: Item created successfully
+ *       400:
+ *         description: Invalid input
+ */
+
+/**
+ * @swagger
+ * /items:
+ *   get:
+ *     summary: Get all Items (with pagination)
+ *     tags: [Items]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Page number
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: Number of items per page
+ *     responses:
+ *       200:
+ *         description: A list of Items
+ */
+
+
 router.post('/',postItemController)
 router.get('/',generatePagination(Items),getItemController)
 router.get("/:id",getOneItemController)
